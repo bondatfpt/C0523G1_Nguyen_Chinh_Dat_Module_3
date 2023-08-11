d3cffea0604ec2aee1eb54865c1ace48dbc728fd
bondatfpt <bondatfpt@gmail.com>
2023-08-11 10:48:37 +0700

    Chinh Dat nop bai tap

--- ss01_relational_database/exercise/exe1.sql
+++ ss01_relational_database/exercise/exe1.sql
@@ -1,15 +1,15 @@
 CREATE DATABASE student_management;
 USE student_management;
-CREATE TABLE class (
+CREATE TABLE classroom (
 	id INT PRIMARY KEY AUTO_INCREMENT,
     `name` varchar(255)
 );
-SELECT*FROM class;
-INSERT INTO class(`name`)
+SELECT*FROM classroom;
+INSERT INTO classroom(`name`)
 VALUES('Chinh Dat');
 
 SET SQL_SAFE_UPDATES = 0;
-UPDATE class
+UPDATE classroom
 SET `name` = 'Bon Dat'
 WHERE `name` = 'Chinh Dat';
 
@@ -18,5 +18,5 @@ id INT PRIMARY KEY AUTO_INCREMENT,
 `name` varchar(50),
 age int,
 country varchar(50)
-)
+);
 
