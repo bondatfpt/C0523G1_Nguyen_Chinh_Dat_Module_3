f5c7d7ac9c844e99251d51a62a8c394c239370d3
bondatfpt <bondatfpt@gmail.com>
2023-08-14 18:55:08 +0700

    Chinh Dat nop bai tap

--- case_study_database_module_3/task_4/task_4.sql
+++ case_study_database_module_3/task_4/task_4.sql
@@ -41,11 +41,13 @@ select
 		loai_khach.ten_loai_khach,hop_dong.ma_hop_dong,
 		dich_vu.ten_dich_vu,
 		hop_dong.ngay_lam_hop_dong, hop_dong.ngay_ket_thuc,
-		chi_phi_thue + ifnull(so_luong,0) * ifnull(gia,0) as tong_tien
+		sum(chi_phi_thue + ifnull(so_luong,0) * ifnull(gia,0)) as tong_tien
 from khach_hang
 join loai_khach on loai_khach.ma_loai_khach = khach_hang.ma_loai_khach
 join hop_dong on khach_hang.ma_khach_hang = hop_dong.ma_khach_hang
 join dich_vu on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
 left join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
-left join dich_vu_di_kem on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem;
+left join dich_vu_di_kem on dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
+group by khach_hang.ma_khach_hang, hop_dong.ma_hop_dong
+order by khach_hang.ma_khach_hang;
 
