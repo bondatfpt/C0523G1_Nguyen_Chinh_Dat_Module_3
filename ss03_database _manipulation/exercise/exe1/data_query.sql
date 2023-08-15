d1a9ab1ab1e7796c0440e9c7bea7ca0b9a7a1448
bondatfpt <bondatfpt@gmail.com>
2023-08-15 10:43:33 +0700

    Chinh Dat nop bai tap

--- ss03_database _manipulation/exercise/exe1/data_query.sql
+++ ss03_database _manipulation/exercise/exe1/data_query.sql
@@ -1,11 +1,11 @@
 use quan_ly_sinh_vien;
-select* from student;
+select* from students;
 select* from classes;
-select* from subject;
-select* from mark;
+select* from subjects;
+select* from marks;
 
 -- Hiển thị tất cả các sinh viên có tên bắt đầu bảng ký tự ‘h’
-select* from student
+select* from students
 where student_name like 'H%'; 
 
 -- Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
@@ -13,23 +13,23 @@ select* from classes
 where month(start_date) = 12;
 
 -- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5.
-select* from `subject`
+select* from subjects
 where credit between 3 and 5;
 
 -- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
 set sql_safe_updates = 0;
-update student
+update students
 set class_id = 2
 where student_name = 'Hung';
 set sql_safe_updates = 1;
 
 -- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
-select student_name, `subject`.sub_name, mark.mark
-from student
-left join mark
-on student.student_id = mark.student_id
-left join `subject`
-on mark.sub_id = `subject`.sub_id
-order by mark.mark desc;
+select student_name, subjects.sub_name, marks.mark
+from students
+left join marks
+on students.student_id = marks.student_id
+join subjects
+on marks.sub_id = subjects.sub_id
+order by marks.mark desc, students.student_name asc;
 
 
