fd75fd35532bdd269334ecb496e9644105fed94a
bondatfpt <bondatfpt@gmail.com>
2023-08-15 21:52:06 +0700

    Chinh Dat nop bai tap

--- case_study_database_module_3/task_4/task_4.sql
+++ case_study_database_module_3/task_4/task_4.sql
@@ -18,13 +18,14 @@ where
 (subdate(curdate(), interval 18 year) >= ngay_sinh 
 and (subdate(curdate(), interval 50 year) <= ngay_sinh ))
 and
-(( dia_chi like '%Đà Nẵng%'
-or dia_chi like '%Quảng Trị%'));
+(( dia_chi like '%Đà Nẵng'
+or dia_chi like '%Quảng Trị'));
 
 -- 4.	Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. 
 -- Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng.
 -- Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
-select khach_hang.ma_khach_hang,khach_hang.ho_va_ten,count(hop_dong.ma_hop_dong) as so_lan_dat_phong
+select khach_hang.ma_khach_hang,khach_hang.ho_va_ten,
+count(hop_dong.ma_hop_dong) as so_lan_dat_phong
 from khach_hang
 left join loai_khach on loai_khach.ma_loai_khach = khach_hang.ma_loai_khach
 left join hop_dong on hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
