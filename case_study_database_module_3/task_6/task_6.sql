f91f036802681751727e14f30ff1a1192c80cf8b
bondatfpt <bondatfpt@gmail.com>
2023-08-16 21:16:59 +0700

    Chinh Dat nop bai tap

--- case_study_database_module_3/task_6/task_6.sql
+++ case_study_database_module_3/task_6/task_6.sql
@@ -67,10 +67,14 @@ having so_lan_su_dung = 1;
 select nhan_vien.ma_nhan_vien, nhan_vien.ho_va_ten,
 		trinh_do.ten_trinh_do, bo_phan.ten_bo_phan,
         nhan_vien.so_dien_thoai, nhan_vien.dia_chi,
-        hop_dong.ngay_lam_hop_dong
+        hop_dong.ngay_lam_hop_dong, count(hop_dong.ma_nhan_vien) as so_lan_lam_hop_dong
 from nhan_vien
 join trinh_do on trinh_do.ma_trinh_do = nhan_vien.ma_trinh_do
 join bo_phan on bo_phan.ma_bo_phan = nhan_vien.ma_bo_phan
 join hop_dong on hop_dong.ma_nhan_vien = nhan_vien.ma_nhan_vien
-group by nhan_vien.ma_nhan_vien
+where year(ngay_lam_hop_dong) between 2020 and 2021
+group by hop_dong.ma_nhan_vien
+having so_lan_lam_hop_dong < 4;
+
+
 
