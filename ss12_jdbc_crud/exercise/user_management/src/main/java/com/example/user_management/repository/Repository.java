4731281566ec4b4ece9303071d1dd461d0c22e6e
bondatfpt <bondatfpt@gmail.com>
2023-08-31 09:51:49 +0700

    form dang nhap

--- ss12_jdbc_crud/exercise/user_management/src/main/java/com/example/user_management/repository/Repository.java
+++ ss12_jdbc_crud/exercise/user_management/src/main/java/com/example/user_management/repository/Repository.java
@@ -9,29 +9,30 @@ import java.util.Comparator;
 import java.util.List;
 
 public class Repository implements IRepository {
-    private static final String SELECT ="call getAll();";
+    private static final String SELECT = "call getAll();";
     private static final String INSERT = "INSERT INTO users(name,email,country)\n" +
-                                        "VALUES(?,?,?)";
+            "VALUES(?,?,?)";
     private static final String DELETE = "call deleteUser(?)";
     private static final String GETUSERBYID = "SELECT * FROM users\n" +
-                          "WHERE id =  ?";
+            "WHERE id =  ?";
     private static final String UPDATE = "call updateUser (?,?,?,?)";
-    private static final String SEARCH ="SELECT users.name " +
+    private static final String SEARCH = "SELECT users.name " +
             "FROM users " +
             "WHERE country LIKE ?";
+
     @Override
     public List<User> getAll() {
-        List <User> userList = new ArrayList<>();
+        List<User> userList = new ArrayList<>();
         Connection connection = BaseRepository.getConnection();
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(SELECT);
-            ResultSet resultSet =  preparedStatement.executeQuery();
-            while (resultSet.next()){
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
                 int id = resultSet.getInt(1);
                 String name = resultSet.getString(2);
                 String email = resultSet.getString(3);
                 String country = resultSet.getString(4);
-                userList.add(new User(id,name,email,country));
+                userList.add(new User(id, name, email, country));
             }
             connection.close();
         } catch (SQLException e) {
@@ -44,14 +45,32 @@ public class Repository implements IRepository {
     public void save(User user) {
         Connection connection = BaseRepository.getConnection();
         try {
+            connection.setAutoCommit(false);
             PreparedStatement preparedStatement = connection.prepareStatement(INSERT);
-            preparedStatement.setString(1,user.getName());
-            preparedStatement.setString(2,user.getEmail());
-            preparedStatement.setString(3,user.getCountry());
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, user.getCountry());
             preparedStatement.executeUpdate();
-            connection.close();
+            if (connection != null) {
+                connection.commit();
+            }
         } catch (SQLException e) {
+            try {
+                if (connection != null) {
+                    connection.rollback();
+                }
+            } catch (SQLException exception) {
+                throw new RuntimeException(exception);
+            }
             throw new RuntimeException(e);
+        } finally {
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
@@ -60,7 +79,7 @@ public class Repository implements IRepository {
         Connection connection = BaseRepository.getConnection();
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(DELETE);
-            preparedStatement.setInt(1,id);
+            preparedStatement.setInt(1, id);
             preparedStatement.executeUpdate();
             connection.close();
         } catch (SQLException e) {
@@ -73,9 +92,9 @@ public class Repository implements IRepository {
         Connection connection = BaseRepository.getConnection();
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(GETUSERBYID);
-            preparedStatement.setInt(1,id);
+            preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 return new User(resultSet.getInt(1),
                         resultSet.getString(2),
                         resultSet.getString(3),
@@ -93,10 +112,10 @@ public class Repository implements IRepository {
         Connection connection = BaseRepository.getConnection();
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE);
-            preparedStatement.setInt(1,id);
-            preparedStatement.setString(2,name);
-            preparedStatement.setString(3,email);
-            preparedStatement.setString(4,country);
+            preparedStatement.setInt(1, id);
+            preparedStatement.setString(2, name);
+            preparedStatement.setString(3, email);
+            preparedStatement.setString(4, country);
             preparedStatement.executeUpdate();
             connection.close();
         } catch (SQLException e) {
@@ -107,12 +126,12 @@ public class Repository implements IRepository {
     @Override
     public List<User> getUserByCountry(String country) {
         Connection connection = BaseRepository.getConnection();
-        List <User> userList = new ArrayList<>();
+        List<User> userList = new ArrayList<>();
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(SEARCH);
-            preparedStatement.setString(1,country+"%");
+            preparedStatement.setString(1, country + "%");
             ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()){
+            while (resultSet.next()) {
                 String name = resultSet.getString("name");
                 userList.add(new User(name));
             }
